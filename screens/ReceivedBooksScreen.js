c312885642b9ff962e42ea17f4ad61a3d4eb880b
Sasmitha3001 <65769282+Sasmitha3001@users.noreply.github.com>
2021-02-15 12:27:08 +0530

    Add files via upload

---
+++ screens/ReceivedBooksScreen.js
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import {View,Text} from 'react-native'
+
+export default class ReceivedBooks extends React.Component{
+    constructor(){
+        super()
+    }
+
+    render(){
+        return(
+            <View>
+                <Text>Received Books </Text>
+            </View>
+        )
+    }
+
+}
